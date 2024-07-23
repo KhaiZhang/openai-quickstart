c2328533539cbeddb91b1e82ee9ec3a9b39eed65
ZHANGKH <Khai.zhang@oocl.com>
2024-07-23 23:42:59 +0800

    homework: sales_chatbot

--- langchain/sales_chatbot/sales_chatbot.py
+++ langchain/sales_chatbot/sales_chatbot.py
@@ -1,13 +1,19 @@
 import gradio as gr
+import os
 
 from langchain_openai import OpenAIEmbeddings
 from langchain.chains import RetrievalQA
 from langchain_openai import ChatOpenAI
 from langchain_community.vectorstores import FAISS
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
+
+os.environ["OPENAI_BASE_URL"] = 'https://api.xiaoai.plus/v1'
 
 def initialize_sales_bot(vector_store_dir: str="real_estates_sale"):
-    db = FAISS.load_local(vector_store_dir, OpenAIEmbeddings())
+    db = FAISS.load_local(vector_store_dir, OpenAIEmbeddings(), allow_dangerous_deserialization=True)
     llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
     
     global SALES_BOT    
@@ -34,13 +40,37 @@ def sales_chat(message, history):
         return ans["result"]
     # 否则输出套路话术
     else:
-        return "这个问题我要问问领导"
+        return chat_with_ai_for_unknow_question(message=message) or "这个问题我要问问领导"
+
+def chat_with_ai_for_unknow_question(message):
+    model = ChatOpenAI(model="gpt-3.5-turbo-0125")
+    prompt = ChatPromptTemplate.from_template("""你是一个专业的汽车销售，现在在与客人进行汽车销售交谈，但是客户问了你一个问题，
+    你却一时不知道答案，这个时候你需要用你过硬的销售话术婉转的回答客人且不能让客人丧失兴趣。客人的问题是： {q} """)
+    output_parser = StrOutputParser()
+    chain = prompt | model | output_parser
+    result = chain.invoke({"q": message})
+    print(f"[ChatResult]{result}")
+    return result
     
+def reload_faiss(path):
+    with open("real_estate_sales_data.txt", encoding='utf-8') as f:
+        real_estate_sales = f.read()
+    text_splitter = CharacterTextSplitter(        
+    separator = r'\d+\.',
+    chunk_size = 100,
+    chunk_overlap  = 0,
+    length_function = len,
+    is_separator_regex = True,)
+    docs = text_splitter.create_documents([real_estate_sales])
+    embeddings = OpenAIEmbeddings()
+    db = FAISS.from_documents(docs, embeddings)
+    db.save_local(path)
+
 
 def launch_gradio():
     demo = gr.ChatInterface(
         fn=sales_chat,
-        title="房产销售",
+        title="汽车销售",
         # retry_btn=None,
         # undo_btn=None,
         chatbot=gr.Chatbot(height=600),
@@ -49,7 +79,12 @@ def launch_gradio():
     demo.launch(share=True, server_name="0.0.0.0")
 
 if __name__ == "__main__":
-    # 初始化房产销售机器人
-    initialize_sales_bot()
+    # current_dir = os.path.dirname(os.path.abspath(__file__))
+    # faiss_index_path = os.path.join(current_dir, 'real_estates_sale')
+    # initialize_sales_bot(vector_store_dir=faiss_index_path)
+
+    path = "test_data"
+    reload_faiss(path);
+    initialize_sales_bot(vector_store_dir=path)
     # 启动 Gradio 服务
     launch_gradio()
